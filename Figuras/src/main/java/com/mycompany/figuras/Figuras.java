fb02e2f57eaa90e6a1814ad2868fc5646d01e201
Nikolaifan24 <nikolaifan36@gmail.com>
2023-09-04 19:27:28 -0500

    agregar nuevo proyecto

---
+++ Figuras/src/main/java/com/mycompany/figuras/Figuras.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.figuras;
+
+/**
+ *
+ * @author nikol
+ */
+public class Figuras {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
